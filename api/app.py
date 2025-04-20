494de62899b1a5df04195ed923e7a277930be3c9
Jatinder Sahota <jsahota751@gmail.com>
2025-04-20 02:54:31 -0500

    Fix CORS problem

--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, send_from_directory, request, jsonify
+from flask_cors import CORS
 import networkx as nx
 import random
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def serve_script():
